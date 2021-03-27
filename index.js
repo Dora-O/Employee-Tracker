c880c47e8b60a06cd0d3e1f13a8c3d7e77a21adf
Dora O <dorozco2390@utexas.edu>
2021-03-26 20:35:23 -0500

    added consts for necessary functions

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//credentials for server conneciton 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3001,
@@ -12,3 +13,56 @@ const connection = mysql.createConnection({
     password: '',
     database: 'employee_db'
 });
+
+const mainMenu = () => {
+    inquirer
+    .prompt({
+        name: 'menu',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View Departments',
+            'Add Departments',
+            'View Roles',
+            'Add Roles',
+            'View Employees',
+            'Add Employees',
+            'Update Employee Roles',
+            'Exit'
+        ]
+    }).then(answers =>{
+        switch(answers.menu){
+        case 'View Departments':
+            viewDepartments();
+            break;
+        case 'Add Departments':
+            addDepartments();
+            break;
+        case 'View Roles':
+            viewRoles();
+            break;
+        case 'Add Roles':
+            addRoles();
+            break;
+        case 'View Employees':
+            viewEmployees();
+            break;
+        case 'Add Employees':
+            addEmployees();
+            break;
+        case 'Update Employee Roles':
+            updateEmployeeRoles();
+            break;
+        case 'Exit':
+            console.log('Goodbye');
+        };
+    });
+};
+
+const addDepartments = () => {};
+const viewDepartments = () => {};
+const viewRoles = () => {};
+const addRoles = () => {};
+const addEmployees = () => {};
+const viewEmployees = () => {};
+const updateEmployeeRoles = () => {};
