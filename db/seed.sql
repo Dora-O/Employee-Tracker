21f50330fd08846c8c414b4ba6cbb35426ba7490
Dora O <dorozco2390@utexas.edu>
2021-03-27 15:03:24 -0500

    added seeds to database

--- db/seed.sql
+++ db/seed.sql
@@ -26,3 +26,13 @@ CREATE TABLE department (
     department_name VARCHAR (30),
     PRIMARY KEY (department_id)
 );
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Tony", "Stark", 1, 00000), ("Pepper", "Potts", 1, 00000);
+
+INSERT INTO department (department_id, department_name)
+VALUES (1, "Admin"), (2, "Engineering"), (3, "Sales");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", 300000.00, 1), ("Front-End Developer", 250000.00, 2), ("Back-End Developer", 250000.00, 2),("Engineer", 300000.00, 2), 
+("Salesperson", 100000.00, 3), ("Social Media Specialist", 200000.00, 1);
